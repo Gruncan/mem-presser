a86906301ec17fac2461bc7dcecd33c066ef12b5
Duncan Jones <dj223@outlook.com>
2024-10-10 11:05:05 +0100

    Allocator accepts size as input

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
-int main(){
-    off_t length = 500 * 1024 * 1024;
+int main(int argc, char *argv[]){
+
+    if (argc <= 1 || argc > 2) {
+        printf("Usage: %s [size in mb]\n", argv[0]);
+        return 1;
+    }
+
+    int size = atoi(argv[1]);
+
+    size_t length = size * 1024 * 1024;
     void* ptr = mmap(NULL, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
     if(ptr == MAP_FAILED){
