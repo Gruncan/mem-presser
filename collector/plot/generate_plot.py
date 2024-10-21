7a2aa629c463aa44fe41e0c7ff87a810640c388f
Duncan Jones <dj223@outlook.com>
2024-10-21 21:16:17 +0100

    Updated generate_plot.py to draw multiple subplots and hover over

--- collector/plot/generate_plot.py
+++ collector/plot/generate_plot.py
@@ -1,17 +1,19 @@
 from datetime import datetime
-
-import matplotlib.pyplot as plt
 import sys
-import os
-
 import json
+import matplotlib.pyplot as plt
+import mplcursors
 
 
-def generate_plot(x, y):
 
+def generate_plot(time, *stats):
     plt.figure()
 
-    plt.plot(x, y, label="Memory Usage vs Time", color="blue", marker="o", linestyle="-")
+    lines = []
+
+    for name, stat in stats:
+        line, = plt.plot(time, stat, label=name)
+        lines.append(line)
 
     plt.title("Memory Usage")
     plt.xlabel("Time")
@@ -19,7 +21,35 @@ def generate_plot(x, y):
 
     plt.legend()
 
+    cursor = mplcursors.cursor(lines, hover=True, annotation_kwargs={'arrowprops': None})
+
+    @cursor.connect("add")
+    def on_click(sel):
+        for l in lines:
+            l.set_linewidth(1)
+            l.set_alpha(0.7)
+            l.set_markeredgewidth(1)
+
+        selected_line = sel.artist
+        selected_line.set_linewidth(3)
+        selected_line.set_alpha(1.0)
+        selected_line.set_markeredgewidth(3)
+
+        plt.gcf().canvas.draw()
+
+    @cursor.connect("add")
+    def on_hover(sel):
+        x, y = sel.target
+
+        sel.annotation.set_text(f'{sel.artist.get_label()}: {y:.2f}')
+
+        bbox = sel.annotation.get_bbox_patch()
+        if bbox is not None:
+            bbox.set(fc="white", alpha=0.6)
+
     plt.show()
+
+
 
 
 
@@ -32,7 +62,13 @@ def main():
     filename = args[0]
 
     x = []
-    y = []
+    sk = 'Memory Used'
+    stats = (sk, "buffers", "cached", "swapCache", "active", "inActive", "swapTotal", "swapFree", "zswap",
+             'zswapped', 'dirty', 'writeback', 'pagesAnon', 'pageMapped', 'kernelStack', 'pageTables',
+             'bounce', 'vmallocChunk', 'perCPU')
+
+    stats = {key: list() for key in stats}
+
 
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -41,10 +77,13 @@ def main():
             key = list(d.keys())[0]
             values = d[key]
             x.append(datetime.strptime(key, '%Y-%m-%d %H:%M:%S.%f'))
-            y.append(int(values["total"]) - int(values["free"]))
+            stats[sk].append((int(values["total"]) - int(values["free"])))
+            for k in stats.keys():
+                v = values.get(k, None)
+                if v is not None:
+                    stats[k].append(int(v))
 
-
-    generate_plot(x, y)
+    generate_plot(x, *tuple(stats.items()))
 
 
 
