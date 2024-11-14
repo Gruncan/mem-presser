4bbc8d2e5bd57674fd3deece624b087f19df3227
Duncan Jones <dj223@outlook.com>
2024-11-14 16:13:35 +0000

    We now exhaust stack

--- main.c
+++ main.c
@@ -1,67 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/sysinfo.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+static int count = 0;
+
+
+void* inf_recursive(void* buffer, size_t size) {
+    if (size < 0) return buffer;
+
+    char new_buffer[size];
+
+    memset(new_buffer, 0, size);
+
+    usleep(100000);
+    count++;
+    printf("Allocations: %d\n", count);
+    inf_recursive(new_buffer, size);
+
+}
+
+
+
+
 
 int main(int argc, char *argv[]){
 
     if (argc <= 1 || argc > 2) {
-        printf("Usage: %s [size in mb]\n", argv[0]);
+        printf("Usage: %s [size in kb]\n", argv[0]);
         return 1;
     }
 
     int size = atoi(argv[1]);
-    unsigned long length = size * 1024 * 1024;
+    unsigned long length = size * 512;
 
-    struct sysinfo info;
-    if (sysinfo(&info) != 0) {
-        perror("sysinfo");
-    }
-
-    unsigned long long freeRamStart = info.freeram;
-
-    void **ptrs = malloc(256 * sizeof(void*));
-    int count = 0;
-    int allocation_times = 0;
-
-    unsigned long l2 = length;
-
-    while (1) {
-
-        ptrs[allocation_times] = malloc(l2);
-        if (ptrs[allocation_times] == NULL) {
-            printf("Memory allocation failed after %d MB\n", count * 10);
-            break;
-        }
-        memset(ptrs[allocation_times], 0, l2);
-
-        printf("Allocated %d\n", allocation_times);
-        allocation_times++;
-
-        usleep(100000); // 100 ms
-
-        if (sysinfo(&info) != 0) {
-            perror("sysinfo");
-            break;
-        }
-
-
-        if ((double) info.freeram <= 0.3 * (double) freeRamStart && count == 0) {
-            l2 /= 2;
-            count++;
-        }else if ((double) info.freeram <= 0.2 * (double) freeRamStart && count == 1) {
-            l2 /= 2;
-            count++;
-        }else if ((double) info.freeram <= 0.1 * (double) freeRamStart && count == 2) {
-            for(int i = 0; i < allocation_times; i++) {
-                free(ptrs[i]);
-            }
-            allocation_times = 0;
-            l2 = length;
-        }
-
-    }
+    char inital[length];
+    inf_recursive(inital, length);
 
     return 0;
 }
